41303222764de653be0b6dfc5cdea3630ec26a82
Peter Ryberg <peterryberg@gmail.com>
2024-03-03 10:37:16 +0100

    Add help files

---
+++ GitExplorer/Helpers/Constants.swift
@@ -0,0 +1,11 @@
+struct Constants {
+    
+    struct API {
+        
+        // https://api.github.com/repos/strapi/strapi/releases
+        static let releaseGithubApiReleaseUrl = "https://api.github.com/repos/strapi/strapi/releases"
+            
+        // https://api.github.com/search/repositories?q=stra
+        static let repositoriesGithubApiUrl = "https://api.github.com/search/repositories?q=stra"
+    }
+}
