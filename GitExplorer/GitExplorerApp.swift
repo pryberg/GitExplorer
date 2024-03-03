adab3b2a1cdcd230822cfb0a6571872631bbefbb
Peter Ryberg <peterryberg@gmail.com>
2024-03-03 09:09:52 +0100

    Initial Commit

---
+++ GitExplorer/GitExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExplorerApp.swift
+//  GitExplorer
+//
+//  Created by Peter Ryberg on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
