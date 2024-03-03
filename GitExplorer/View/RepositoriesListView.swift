a595766a0ab14526fde9b3a2441ee6a0b8ed32ea
Peter Ryberg <peterryberg@gmail.com>
2024-03-03 13:01:02 +0100

    Updated for Detail of Repository

--- GitExplorer/View/RepositoriesListView.swift
+++ GitExplorer/View/RepositoriesListView.swift
@@ -15,15 +15,51 @@ struct RepositoriesListView: View {
                     
                     List(viewModel.repositoriesArray) { repos in
                         NavigationLink {
-                            VStack(alignment: .leading) {
-                                Text(repos.name ?? "")
+                            VStack(alignment: .center) {
+                                AsyncImage(url: URL(string: repos.owner?.avatarUrl ?? ""), scale: 2)
+                                Text("\(repos.owner?.login ?? "") / \(repos.name ?? "")")
+                                Text(repos.language ?? "")
+                                
+                                VStack {
+                                    HStack {
+                                        Text("Forks")
+                                        Spacer()
+                                        Text(String(repos.forksCount ?? 0))
+                                    }
+                                    
+                                    HStack {
+                                        Text("Issues")
+                                        Spacer()
+                                        Text(String(repos.openIssuesCount ?? 0))
+                                    }
+                                    
+                                    HStack {
+                                        Text("Stared by")
+                                        Spacer()
+                                        Text(String(repos.stargazersCount ?? 0))
+                                    }
+                                    
+                                    HStack {
+                                        Text("Last release version")
+                                        Spacer()
+                                        Text("327")
+                                    }
+//                                    Text(repos.releasesUrl ?? "")
+                                }
+                                .padding(20)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 16)
+                                        .stroke(.gray, lineWidth: 0.5)
+                                )
+                                .padding(20)
+                                                                
+                                Spacer()
                             }
                             
                         } label: {
                             ReposRow(repos: repos).padding(0)
                         }
                     }
-                    
                 } else {
                     EmptyListView()
                 }
